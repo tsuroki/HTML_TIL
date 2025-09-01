bfa2d0845b5309504da66080d7aa701f0f5149e8
tsuroki <tsuroki0210@gmail.com>
2025-09-01 09:41:41 +0900

    footer php 제작

---
+++ figma_layout/samlip/footer.php
@@ -0,0 +1,49 @@
+<footer>
+    <div class="top">
+        <div class="menu">
+            <a href="#"><span>개인정보 처리방침</span></a>
+            <a href="#"><span>영상정보처리기기 운영관리방침</span></a>
+            <a href="#"><span>거래희망회사 사전등록</span></a>
+            <a href="#"><span>동반성장</span></a>
+            <img src="./images/customer.png" alt="소비자중심CCM">
+        </div>
+        <ul name="site" id="site">
+            <li>
+                <p>FAMILY SITE</p>
+                <ul class="family_list">
+                    <li><a href="#">PALIS BAGUETEE</a></li>
+                    <li><a href="#">BASKIN ROBBINS</a></li>
+                    <li><a href="#">DUNKIN</a></li>
+                    <li><a href="#">PARIS CORISSANT</a></li>
+                    <li><a href="#">PALIS BAGUETEE</a></li>
+                    <li><a href="#">BASKIN ROBBINS</a></li>
+                    <li><a href="#">DUNKIN</a></li>
+                    <li><a href="#">PARIS CORISSANT</a></li>
+                    <li><a href="#">PALIS BAGUETEE</a></li>
+                    <li><a href="#">BASKIN ROBBINS</a></li>
+                    <li><a href="#">DUNKIN</a></li>
+                    <li><a href="#">PARIS CORISSANT</a></li>
+                </ul>
+            </li>
+            <li>
+                <p>SITE MAP</p>
+                <div class="sitemap_wrap">
+                    <div class="sitemap_contents">
+                        <h2>SITE MAP</h2>
+                        <button type="button" class="close">X</button>
+                        <!-- 사이트 맵 목록 작성위치 -->
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="company_info">
+        <address class="com_ad">
+            Customer Service 전화번호 080-739-8572 (월~금 09:00~17:00)<br>
+            101, Gongdan 1-daero, Siheung-si, Gyeonggi-do, Republic of Korea ㈜SPC삼립 대표이사 황종현
+        </address>
+        <p id="copy">
+            &copy; SPC Samlip. All rights reserved.
+        </p>
+    </div>
+</footer>
